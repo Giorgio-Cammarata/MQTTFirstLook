65c664550320e9d1309097b4870b4a694312197d
Giorgio-Cammarata <giorgio.cammarata.jb@gmail.com>
2021-10-19 15:34:11 +0200

    MQTTFirstLook repo

---
+++ MQTTFirstLook.Broker/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using MQTTnet;
+using MQTTnet.Server;
+using Serilog;
+
+namespace MQTTFirstLook.Broker
+{
+    /* Broker implementation
+     * 
+     * This small broker implementation uses the MQTT protocol. 
+     * (Message Queue Telemetry Transport, publish-subscribe network protocol).
+     * The broker has the responsibility to read data received from connected clients and display it on the screen.
+     * For this, we're going to create a new MQTTServer that listen to the port 707 on localhost.
+     */
+
+    class Program
+    {
+        private static int _messageCounter = 0;
+
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            #region Implementation
+
+            /* Create the options for the MQTT Broker:
+             * - set endpoint to localhost;
+             * - port used will be 707;
+             * - handler for new connections;
+             * - handler for new messages.
+             */
+            MqttServerOptionsBuilder options = new MqttServerOptionsBuilder()
+                .WithDefaultEndpoint()
+                .WithDefaultEndpointPort(707)
+                .WithConnectionValidator(OnNewConnection)
+                .WithApplicationMessageInterceptor(OnNewMessage);
+
+            // Creates a new MQTT server     
+            IMqttServer mqttServer = new MqttFactory().CreateMqttServer();
+
+            // Start the server with options  
+            mqttServer.StartAsync(options.Build()).GetAwaiter().GetResult();
+
+            // Keep application running until user press a key
+            Console.ReadLine();
+
+            #endregion
+        }
+
+        #region Handlers
+
+        /* Handlers are callbacks that MQTT calls whenever an action is called.
+         * In this example, we have 2 handler for:
+         * - new connections;
+         * - whenever the server gets a new message.        
+         */
+
+        public static void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            Log.Logger.Information(
+                    "New connection: ClientId = {clientId}, Endpoint = {endpoint}, CleanSession = {cleanSession}",
+                    context.ClientId,
+                    context.Endpoint,
+                    context.CleanSession);
+        }
+
+        public static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+            _messageCounter++;
+
+            Log.Logger.Information(
+                "MessageId: {MessageCounter} - TimeStamp: {TimeStamp} -- Message: ClientId = {clientId}, Topic = {topic}, Payload = {payload}, QoS = {qos}, Retain-Flag = {retainFlag}",
+                _messageCounter,
+                DateTime.Now,
+                context.ClientId,
+                context.ApplicationMessage?.Topic,
+                payload,
+                context.ApplicationMessage?.QualityOfServiceLevel,
+                context.ApplicationMessage?.Retain);
+        }
+
+        #endregion
+    }
+}
---
+++ MQTTFirstLook.Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace MQTTFirstLook.Client
+{
+    /* Client implementation
+     * 
+     * In this client, we're going to create a new MQTTClient instance. 
+     * This instance will connect to our Broker on localhost:707 
+     * and send messages to the topic Dev.to/topic/json
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            #region Implementation
+
+            // Creates a new client (MQTTClient will coonnect to our broker via TCP)
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                        .WithClientId("Test.ClientId")
+                                        .WithTcpServer("localhost", 707);
+
+            // Create client options objects
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            // Creates the client object
+            IManagedMqttClient mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            // Set up handlers
+            mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(
+                a =>
+                {
+                    Log.Logger.Information("Message recieved: {payload}", a.ApplicationMessage);
+                });
+
+            // Starts a connection with the Broker
+            mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            // Send a new message to the broker every 2 seconds
+            while (true)
+            {
+                string json = JsonConvert.SerializeObject(
+                    new
+                    {
+                        message = "Test - client message",
+                        sent = DateTimeOffset.UtcNow
+                    });
+
+                mqttClient.PublishAsync("Test.ClientId/topic/json", json);
+
+                Task.Delay(2000).GetAwaiter().GetResult();
+            }
+
+            #endregion
+        }
+
+        #region Handlers
+
+        /* The client Handlers are called when the client: 
+         * - recieves a signal that is connection
+         * - connection failed 
+         * - when is disconnected         
+         */
+
+        public static void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected.");
+        }
+
+        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker.");
+        }
+
+        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected.");
+        }
+
+        #endregion
+    }
+}
